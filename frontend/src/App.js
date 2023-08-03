51798119a9760fe93669d43b0a44c2a3098818a0
AkoruuDev <akoruu.dev@gmail.com>
2023-08-02 23:13:57 -0300

    feat: update App file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import { SignIn, SignUp } from './pages';
+import GlobalStyle from './styles/globalStyles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <></>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <SignIn /> } />
+          <Route path="/sign-up" element={ <SignUp /> } />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
