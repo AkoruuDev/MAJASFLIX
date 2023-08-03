4a2f3d4f02f501af3d0685e834d1fdd22d477bfd
AkoruuDev <akoruu.dev@gmail.com>
2023-08-03 00:03:50 -0300

    feat: update SignIn page

--- frontend/src/pages/log/SignIn.js
+++ frontend/src/pages/log/SignIn.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { ButtonForm, Container, Input, Link } from "../../styles/components";
+import { ButtonForm, Container, Bg, Bgd, Input, Link, Title_tl, CBox, InputBox } from "../../styles/components";
 import styled from 'styled-components';
 import { useNavigate } from "react-router-dom";
-import bgi from "../../assets/bgi.png"
+import bgi from "../../assets/bgi.png";
 
 function SignIn () {
     const [login, setLogin] = useState({});
@@ -22,72 +22,36 @@ function SignIn () {
 
     return (
         <Container>
-            <Title>MAJASFLIX</Title>
-            <Bg image={bgi}>
-                <Bgd></Bgd>
-                <Box>
-                    <h1>Entrar</h1>
-                    <FormLog onSubmit={saveLog}>
+            {/* eslint-disable-next-line react/jsx-pascal-case */}
+            <Title_tl>MAJASFLIX</Title_tl>
+            <Bg image={bgi}></Bg>
+            <Bgd></Bgd>
+            <CBox>
+                <h1>Entrar</h1>
+                <FormLog onSubmit={saveLog}>
+                    <InputBox>
                         <Input type="email" name="email" placeholder="email" onChange={e => {
                             getLog(
                                 e.target.name,
                                 e.target.value
                             )
                         }} required />
-                        <div>
-                            <Input type="password" name="password" placeholder="senha" onChange={e => {
-                                getLog(
-                                    e.target.name,
-                                    e.target.value
-                                )
-                            }} required/>
-                            <ButtonForm type="submit">Acessar</ButtonForm>
-                        </div>
-                        <Link onClick={() => navigate('/sign-up')}>Não tem uma conta?</Link>
-                    </FormLog>
-                </Box>
-            </Bg>
+                    </InputBox>
+                    <InputBox>
+                        <Input type="password" name="password" placeholder="senha" onChange={e => {
+                            getLog(
+                                e.target.name,
+                                e.target.value
+                            )
+                        }} required/>
+                        <ButtonForm type="submit">Acessar</ButtonForm>
+                    </InputBox>
+                    <Link onClick={() => navigate('/sign-up')}>Não tem uma conta?</Link>
+                </FormLog>
+            </CBox>
         </Container>
     )
-}
-
-const Title = styled.h1`
-    position: absolute;
-    left: 32px;
-    top: 32px;
-    color: #ffd700;
-    z-index: 1;
-`;
-
-const Bg = styled.div`
-    height: 100vh;
-    width: 100vw;
-    background: url(${props => props.image});
-
-    display: flex;
-    justify-content: center;
-    z-index: -1;
-`;
-
-const Bgd = styled.div`
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    background-image: linear-gradient(#ffd70012, #000000);
-    opacity: .7;
-    z-index: -1;
-`;
-
-const Box = styled.div`
-    width: 60%;
-    height: 100vh;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    color: #dddddd;
-`;
+};
 
 const FormLog = styled.form`
     width: 80%;
@@ -96,12 +60,6 @@ const FormLog = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
-
-    & div {
-        width: 100%;
-        display: flex;
-        align-items: center;
-    }
 `;
 
 export {
