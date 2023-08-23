181a6fb1e8eb1ee32dc456e302dbb69a913a524e
AkoruuDev <akoruu.dev@gmail.com>
2023-08-23 20:11:49 -0300

    chore: get list from api

--- frontend/src/pages/nav/Movie.js
+++ frontend/src/pages/nav/Movie.js
@@ -1,47 +1,51 @@
 /* eslint-disable react/jsx-pascal-case */
 /* eslint-disable jsx-a11y/heading-has-content */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { Embed_icon, Embed_title } from "../../styles/components";
 import { CiPlay1 } from 'react-icons/ci';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 import { useNavigate } from 'react-router-dom';
+import { takeAllMovie } from "../../database/api";
 
 function Movie() {
     const [gen, setGen] = useState([1, 1]);
-    const [movie, setMovie] = useState({
-        link: "https://rr3---sn-bg07dnrd.c.drive.google.com/videoplayback?expire=1691113465&ei=uR_MZOHtI7PA2LYPttK-4AI&ip=2804:5358:8010:38:1148:3c8e:7e63:d51e&cp=QVROU0JfUlRURlhPOjN4dlVMZ1BzVUlfNkpQYVktdWFsSjRNemg1WXBLMW0waDV2VUpzQWVvano&id=78671ba5de40d7e6&itag=18&source=webdrive&requiressl=yes&mh=95&mm=32&mn=sn-bg07dnrd&ms=su&mv=u&mvi=3&pl=32&ttl=transient&susc=dr&driveid=1oFN_tZlf0CL_t1T4EZ7gYKby-vjP8-BD&app=explorer&mime=video/mp4&vprv=1&prv=1&dur=6165.966&lmt=1690931872934434&mt=1691098768&subapp=DRIVE_WEB_FILE_VIEWER&txp=0006224&sparams=expire,ei,ip,cp,id,itag,source,requiressl,ttl,susc,driveid,app,mime,vprv,prv,dur,lmt&sig=AOq0QJ8wRAIgNLXh8uO47g1uhEdtfebHUJiPv5mqPADpU60NTtCbfoQCIAHiRTFjrMQhT9SAWVVfyv1pUp4hvHN24De832uIu87a&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRQIhANTUL8dpiyJC3ApNS7CR8BqgjIqB14E0BAO8FMToX41mAiB0blhgsLvr2BGTdVFChS1g3AFsDCqiSGTEq-OHa9wAeQ==&cpn=8yZL8P9baC8jqd6F&c=WEB_EMBEDDED_PLAYER&cver=1.20230730.00.00",
-        name: "Nimona",
-        folder: "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-w8P7rcjEqVZ5U35WPO9x5aXl1DOFyr00FN2uq10MREDyRPWodWro3vAjY-qft-dC8kE3X4UgNI-Zm56GqdDP2SvyW8DQ=w1850-h976",
-        time: "1 hora 42 minutos 45 segundos"
-    });
+    const [movies, setMovies] = useState([{}]);
     const [fav, setFav] = useState(false);
     const navigate = useNavigate()
 
-    function play() {
-        navigate(`/play/${movie.name}/dub`)
+    useEffect(() => {
+        setMovies(takeAllMovie())
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    console.log(movies)
+    function play(thisMovie) {
+        navigate(`/play/${thisMovie.name}/`)
     }
 
     return (
-        <Embed>
-            <Sup>
-                <Bgi src={movie.folder} alt={`movie-${movie.name}`} /> {/* background image */}
-                <Embed_icon>M</Embed_icon> {/* MAJAS icon */}
-                <Embed_title color={'#ff3333'}>Nimona</Embed_title> {/* movie name */}
-            </Sup>
-            <Inf>
-                <div>
-                    <CiPlay1 onClick={() => play()} /> {/* play icon */}
-                    <span onClick={() => setFav(!fav)}>{fav ? <AiFillHeart /> : <AiOutlineHeart />}</span> {/* favoritar */}
-                    <span></span> {/* more */}
+        <>{movies.map(movie => {
+            return (<Embed>
+                <Sup>
+                    <Bgi src={movie.folder} alt={`movie-${movie.name}`} /> {/* background image */}
+                    <Embed_icon>M</Embed_icon> {/* MAJAS icon */}
+                    <Embed_title color={'#ff3333'}>{movie.name}</Embed_title> {/* movie name */}
+                </Sup>
+                <Inf>
                     <div>
-                        {gen.forEach(e => {
-                            <span>{e}</span>
-                        })}
+                        <CiPlay1 onClick={() => play(movie)} /> {/* play icon */}
+                        <span onClick={() => setFav(!fav)}>{fav ? <AiFillHeart /> : <AiOutlineHeart />}</span> {/* favoritar */}
+                        <span></span> {/* more */}
+                        <div>
+                            {gen.forEach(e => {
+                                <span>{e}</span>
+                            })}
+                        </div>
                     </div>
-                </div>
-            </Inf>
-        </Embed>
+                </Inf>
+            </Embed>)
+        })}</>
     );
 };
 
