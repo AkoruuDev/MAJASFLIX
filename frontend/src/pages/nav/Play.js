5e1db44670aafe2a3da157c14791faef02ec4b50
AkoruuDev <akoruu.dev@gmail.com>
2023-08-24 22:50:06 -0300

    chore: upload play page

--- frontend/src/pages/nav/Play.js
+++ frontend/src/pages/nav/Play.js
@@ -1,20 +1,21 @@
+/* eslint-disable react/jsx-pascal-case */
 import { useParams } from "react-router-dom";
 import { Container } from "../../styles/components";
 import { takePlay } from "../../database/api";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { styled } from "styled-components";
 
 function Play() {
     const [movie, setMovie] = useState({
-        link: "https://mflixapi.s3.sa-east-1.amazonaws.com/Nimona.mp4",//"https://www.youtube.com/embed/lx0eir2xF5E?list=PLDj7tcnirYg30Q_br6LOArkH7YbyMySe-",
+        link: "https://mflixapi.s3.sa-east-1.amazonaws.com/filmes/O+Amor+Mandou+Mensagem.mp4",
         name: "Nimona",
         folder: "https://lh3.googleusercontent.com/u/0/drive-viewer/AITFw-w8P7rcjEqVZ5U35WPO9x5aXl1DOFyr00FN2uq10MREDyRPWodWro3vAjY-qft-dC8kE3X4UgNI-Zm56GqdDP2SvyW8DQ=w1850-h976",
         time: "1 hora 42 minutos 45 segundos"
     });
-    const { name, type } = useParams();
+    const { name } = useParams();
     
     useEffect(() => {
-        // setMovie(takePlay(name, type))
+        // setMovie(takePlay(name))
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
@@ -22,34 +23,123 @@ function Play() {
 
     return (
         <Container>
-            <Teste>
-                {/* <iframe
-                    width="640"
-                    height="360"
-                    src={movie.link}
-                    title={movie.name}
-                    allowFullScreen
-                ></iframe> */}
-                <video controls width="640" height="360">
+            <Box>
+                <CustomVideo videoSource={movie.link} />
+            </Box>
+            {/* <Film>
+                <CustomVideo autoPlay controls>
                     <source src={movie.link} type="video/mp4" />
                     Seu navegador não suporta a reprodução de vídeos.
-                </video>
-            </Teste>
+                </CustomVideo>
+                <Controls>
+                    <Button>Play/Pause</Button>
+                    <Button>mute</Button>
+                </Controls>
+            </Film> */}
         </Container>
     );
 };
 
-const Teste = styled.div`
-    width: 200px;
-    height: 200px;
-    background-color: #fff;
+const Box = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    width: 100vw;
+    height: 100vh;
 `
 
-const Video = styled.source`
-    width: 100px;
-    height: 100px;
-    background-color: black;
-`;
+const Film = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+`
+
+const CustomVideo = ({ videoSource }) => {
+    const videoRef = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [isMuted, setIsMuted] = useState(false);
+
+    const togglePlayPause = () => {
+        if (videoRef.current.paused) {
+            videoRef.current.play();
+        } else {
+            videoRef.current.pause();
+        }
+        setIsPlaying(!isPlaying);
+    };
+
+    const toggleMute = () => {
+        videoRef.current.muted = !videoRef.current.muted;
+        setIsMuted(!isMuted);
+    };
+
+    return (
+        <Film>
+            <Video ref={videoRef} autoPlay>
+                <source src={videoSource} type="video/mp4" />
+            </Video>
+            <ControlHover>
+                <Controls>
+                    <Button onClick={togglePlayPause}>{isPlaying ? 'Play' : 'Pause'}</Button>
+                    <Button onClick={toggleMute}>{isMuted ? 'Unmute' : 'Mute'}</Button>
+                </Controls>
+            </ControlHover>
+        </Film>
+    );
+};
+
+const Video = styled.video`
+    width: 100%;
+    height: 100%;
+`
+
+const ControlHover = styled.div`
+    position: absolute;
+
+    height: 100vh;
+    width: 100%;
+
+    bottom: 0px;
+    left: 0;
+
+    background-color: transparent;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+
+
+    &:hover {
+        & div {
+            bottom: 0px;
+        }
+    }
+`
+
+const Controls = styled.div`
+    position: absolute;
+
+    bottom: -80px;
+    left: 0;
+    transition: all 0.5s ease;
+
+    width: 100%;
+    background-color: transparent;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Button = styled.button`
+    background-color: transparent;
+    border: none;
+    color: white;
+    cursor: pointer;
+    margin: 0 10px;
+`
 
 export {
     Play
