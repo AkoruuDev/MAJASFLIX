efe05bafa88bd6b7a026fc6c16fa3d19ee6154d5
AkoruuDev <akoruu.dev@gmail.com>
2023-09-01 13:04:57 -0300

    feat: update styledcomponents

--- frontend/src/styles/components/Embed.js
+++ frontend/src/styles/components/Embed.js
@@ -16,7 +16,39 @@ const Embed_title = styled(Title)`
     color: ${props => props.color};
 `;
 
+const Embed_titleBox = styled.p`
+    font-size: 15px;
+    font-family: Arial, Helvetica, sans-serif;
+    color: ${props => props.color};
+`;
+
+const Embed_button = styled.span`
+    padding: 8px 15px;
+    border-radius: 36px;
+    background-color: #888;
+
+    cursor: pointer;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & > span {
+        margin-left: 5px;
+    }
+`
+
+const Embed_buuttonBox = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    padding: 0 0 15px 0;
+`
+
 export {
     Embed_icon,
-    Embed_title
+    Embed_title,
+    Embed_titleBox,
+    Embed_button,
+    Embed_buuttonBox
 }
